4aac578eb18499d3d9c25206a260ab48f32b4acc
Guiren Anice <anice.guiren@gmail.com>
2025-11-17 16:31:37 +0100

    ajout overlap

--- scripts/transform.py
+++ scripts/transform.py
@@ -1,8 +1,14 @@
+from pydub import AudioSegment
+
 def walking_sound():
     pass
 
-def overlap_sound():
-    pass
+def overlap_sound(base: AudioSegment, sounds: list):
+    mix = base
+    for s in sounds:
+        mix = mix.overlay(s)
+    return mix
+
 
 def volume():
     pass
