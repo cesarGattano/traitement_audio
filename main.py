324ea170935abce0802852f19bde814ebfcf1ebe
Valentin <valentaf1803@gmail.com>
2025-11-17 16:51:58 +0100

    ya tout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import yaml
 from yaml import Loader
-from scripts.load_export import load_voice, load_walking, load_ambiance
+from scripts.load_export import load_voice, load_walking, load_ambiance, export_sound
+from scripts.transform import fade_in, volume, overlap_sound
 from pydub import AudioSegment
 from pydub.playback import play
 
@@ -14,4 +15,19 @@ voice = load_voice(config)
 walking = load_walking(config)
 ambiance = load_ambiance(config)
 
-play(ambiance)
+voice = volume(voice, config["sounds"]["voice"].get("volume", 0))
+walking = volume(walking, config["sounds"]["walking"].get("volume", 0))
+ambiance = volume(ambiance, config["sounds"]["ambiance"].get("volume", 0))
+
+walking = fade_in(walking, duration=9000)
+
+overlap = overlap_sound(ambiance, [walking, voice])
+
+export_sound(
+    overlap,
+    config,
+    name="final_mix",
+    format="mp3",
+)
+
+# play(overlap)
